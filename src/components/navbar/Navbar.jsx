feb07100a2347ec0d7317d69236b9f549a3e5bd0
DJayanga <iit117021@std.uwu.ac.lk>
2022-02-13 13:41:55 +0530

    3

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,17 +1,39 @@
-import React from 'react'
-import './navbar.css';
-import { RiMenu3Line, RiCloseLin } from 'react-icons/ri';
+import React from "react";
+import "./navbar.css";
+import { RiMenu3Line, RiCloseLin } from "react-icons/ri";
+import logo from "../../assests/logo.svg";
 
 const Navbar = () => {
   return (
-    <div className='webt__nav'>
-      <div className='webt__navbar-link'>
-        <div className='webt__navbar-links-logo'>
+    <div className="webt__nav">
+      <div className="webt__navbar-links">
+        <div className="webt__navbar-links_logo">
           <img src={logo} alt="Logo" />
         </div>
+        <div className="webt__navbar-links_container">
+          <p>
+            <a href="#home">Home</a>
+          </p>
+          <p>
+            <a href="#whatWebTemp">What is WebTemp</a>
+          </p>
+          <p>
+            <a href="#possibility">Open AI</a>
+          </p>
+          <p>
+            <a href="#features">Case Studies</a>
+          </p>
+          <p>
+            <a href="#blog">Library</a>
+          </p>
+        </div>
       </div>
+      <div className="webt__navbar-sign">
+        <p>Sign In</p>
+        <button type="button">Sign Up</button>
       </div>
-  )
-}
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
